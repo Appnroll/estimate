73a35906f4af6c8abbd711eb54322c9f2d45203c
Michał Kurczewski <michalkurczewski94@gmail.com>
2019-03-22 14:29:05 +0100

    adding user stories form - first approach

---
+++ pages/estimations/_id/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-col>
+    <app-estimation-builder></app-estimation-builder>
+  </el-col>
+</template>
+
+<script>
+  // import AppProjectTable from '~/components/project/appProjectTable'
+
+  import AppEstimationBuilder from '~/components/forms/estimations/builder/appEstimationBuilder'
+  export default {
+    name: 'EstimationsId',
+    components: {
+      AppEstimationBuilder
+      // AppProjectTable
+    },
+    data () {
+      return {
+        project: {},
+
+      }
+    },
+    mounted () {
+
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
